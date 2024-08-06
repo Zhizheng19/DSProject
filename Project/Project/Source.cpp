7b7c31bb64ac3c3b30e15af25932f08184a75b3f
Dong <Zdong3119@conestogac.on.ca>
2024-08-06 11:30:40 -0400

    Initialized the Project

---
+++ Project/Project/Source.cpp
@@ -0,0 +1,23 @@
+/*
+* FILENAME      : project.cpp
+* PROJECT       : project
+* PROGRAMMER    : Zhizheng Dong
+* FIRST VERSION : 2024-08-06
+* DESCRIPTION	:
+*	This file loads ...
+*/
+
+#pragma warning (disable : 4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HASH_TABLE_SIZE      127
+
+//prototypes
+int main(void) {
+
+
+
+	return 0;
+}
